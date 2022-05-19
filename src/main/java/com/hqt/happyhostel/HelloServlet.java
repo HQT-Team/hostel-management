6f4c299ec01cc012e6a2ec7a85be87793567ddea
ngtanloc <nguyentanloc160201@gmail.com>
2022-05-19 11:34:05 +0700

    loi xin loi

--- src/main/java/com/hqt/happyhostel/HelloServlet.java
+++ src/main/java/com/hqt/happyhostel/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello IntelliJ and Java Core!";
+        message = "Xin loi Ho Hai Nam!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
