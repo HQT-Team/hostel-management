f23f6017308dcb6e5a67215af08d2883dcb0a3cc
Coder.NamDepTrai <honam267@gmail.com>
2022-05-19 10:13:57 +0700

    test2

--- src/main/java/com/hqt/happyhostel/HelloServlet.java
+++ src/main/java/com/hqt/happyhostel/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!"
+        message = "Hello Loc!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
