d9441b660f19f13f4239b63412ff880fc7a6219d
Coder.NamDepTrai <honam267@gmail.com>
2022-05-31 13:30:45 +0700

    Merger Nam + UI v2

--- src/main/java/com/hqt/happyhostel/utils/DBUtils.java
+++ src/main/java/com/hqt/happyhostel/utils/DBUtils.java
@@ -4,21 +4,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBUtils {
-    public static Connection makeConnection() throws Exception{
-        Connection cn=null;
-        String IP="localhost";
-//        String instanceName="CUTEPHOMAIQUE";
-        String instanceName="KHOAHD7621\\KHOAHD";
-        String port="1433";
-        String uid="sa";
-//        String pwd="OIUOiu12";
-        String pwd="0792596763";
-        String db="HostelManagement";
-        String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-        String url="jdbc:sqlserver://" +IP+"\\"+ instanceName+":"+port
-                +";databasename="+db+";user="+uid+";password="+pwd+ ";encrypt=true;trustServerCertificate=true;";
+    public static Connection makeConnection() throws Exception {
+        Connection cn = null;
+        String IP = "localhost";
+        String instanceName = "CUTEPHOMAIQUE";
+        String pwd = "OIUOiu12";
+//        String instanceName="KHOAHD7621\\KHOAHD";
+//        String pwd="0792596763";
+        String port = "1433";
+        String uid = "sa";
+        String db = "HostelManagement";
+        String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        String url = "jdbc:sqlserver://" + IP + "\\" + instanceName + ":" + port
+                + ";databasename=" + db + ";user=" + uid + ";password=" + pwd + ";encrypt=true;trustServerCertificate=true;";
         Class.forName(driver);
-        cn= DriverManager.getConnection(url);
+        cn = DriverManager.getConnection(url);
         return cn;
     }
 }
