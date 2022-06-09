d34a228ee9c3a80032e2feca8520777272a6cb19
ngtanloc <nguyentanloc160201@gmail.com>
2022-06-09 22:50:29 +0700

    Comment Servlet QR

--- src/main/java/com/hqt/happyhostel/dto/Account.java
+++ src/main/java/com/hqt/happyhostel/dto/Account.java
@@ -4,7 +4,6 @@ import lombok.*;
 
 import java.util.ArrayList;
 
-
 @Builder
 @Data
 @AllArgsConstructor
--- src/main/java/com/hqt/happyhostel/utils/DBUtils.java
+++ src/main/java/com/hqt/happyhostel/utils/DBUtils.java
@@ -7,14 +7,14 @@ public class DBUtils {
     public static Connection makeConnection() throws Exception {
         Connection cn = null;
         String IP = "localhost";
-        String instanceName = "CUTEPHOMAIQUE";
-        String pwd = "OIUOiu12";
+//        String instanceName = "CUTEPHOMAIQUE";
+//        String pwd = "OIUOiu12";
 //        String instanceName = "DESKTOP-FDKP91N\\SQLEXPRESS";
 //        String pwd = "sa123";
         //String instanceName="KHOAHD7621\\KHOAHD";
        // String pwd="0792596763";
-//        String instanceName="KHOAHD7621\\KHOAHD";
-//        String pwd="0792596763";
+        String instanceName="DESKTOP-0D7MP1Q\\TANLOC";
+        String pwd="Tanloc162";
 //        String instanceName="TTPHATS\\TTPHATS";
 //        String pwd="12345";
         String port = "1433";
