e8bae242183f7c2bb8d4b83d2d6911e5e2f74aa0
Coder.NamDepTrai <honam267@gmail.com>
2022-06-25 08:46:54 +0700

    Hai Nam update CreateContractServlet, GetInviteCodeServlet.java

--- src/main/java/com/hqt/happyhostel/servlet/InviteRoomServlets/GetInviteCodeServlet.java
+++ src/main/java/com/hqt/happyhostel/servlet/InviteRoomServlets/GetInviteCodeServlet.java
@@ -26,6 +26,9 @@ public class GetInviteCodeServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+        response.setDateHeader("Expires", 0); // Proxies.
         String url = ERROR;
         int roomID;
         Account owner;
