0bf51e2631ee1f1ab9a36d4a5ab0e4f834c7ec76
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-07-17 20:21:18 +0700

    Add base reset password handler

---
+++ src/main/java/com/hqt/happyhostel/servlet/RecoverPasswordServlets/RecoverPasswordResetServlet.java
@@ -0,0 +1,21 @@
+package com.hqt.happyhostel.servlet.RecoverPasswordServlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "RecoverPasswordResetServlet", value = "/RecoverPasswordResetServlet")
+public class RecoverPasswordResetServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
---
+++ src/main/java/com/hqt/happyhostel/servlet/RecoverPasswordServlets/RecoverPasswordServlet.java
@@ -0,0 +1,22 @@
+package com.hqt.happyhostel.servlet.RecoverPasswordServlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "RecoverPasswordServlet", value = "/RecoverPasswordServlet")
+public class RecoverPasswordServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
+
