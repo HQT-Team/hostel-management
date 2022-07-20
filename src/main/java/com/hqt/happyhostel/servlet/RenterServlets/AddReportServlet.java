dc122c782bf27c175d2bb9ad82344cdfb770d890
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-07-20 22:11:43 +0700

    Update renter send report mechanism

--- src/main/java/com/hqt/happyhostel/servlet/RenterServlets/AddReportServlet.java
+++ src/main/java/com/hqt/happyhostel/servlet/RenterServlets/AddReportServlet.java
@@ -14,36 +14,35 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "AddReportServlet", value = "/AddReportServlet")
 public class AddReportServlet extends HttpServlet {
-    public static final String ERROR = "Renter-report-page";
-    public static final String SUCCESS = "Renter-report-page";
-//    public static final String SUCCESS = "Renter-view-report";
-
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //req.getRequestDispatcher("Renter-report-page").forward(req, resp);
+        try {
+            List<ReportCategory> reportCategories = new ReportCategoryDAO().getReportCategory();
+            req.setAttribute("REPORT_CATE", reportCategories);
+        } catch (Exception e) {
+            log("Error at GetReportCategoryListServlet: " + e.toString());
+        } finally {
+            req.getRequestDispatcher("Renter-report-page").forward(req, resp);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String url = ERROR;
+        req.setCharacterEncoding("UTF-8");
 
         LocalDate dateObj = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String sendDate = dateObj.format(formatter);
-        Account acc = new Account();
-
-        HandlerStatus handlerStatus = null;
+        HandlerStatus handlerStatus;
 
         try {
-            req.setCharacterEncoding("UTF-8");
             HttpSession session = req.getSession();
-            acc = (Account) session.getAttribute("USER");
+            Account acc = (Account) session.getAttribute("USER");
 
             int accountId = acc.getAccId();
             int cateID = Integer.parseInt(req.getParameter("cateID"));
@@ -58,21 +57,27 @@ public class AddReportServlet extends HttpServlet {
                     .replyAccountID(ownerID)
                     .cateID(cateID)
                     .build();
+
             //Add report
             int reportId = new ReportDAO().addReport(report);
+
             if (reportId > 0){
                 req.setAttribute("SUCCESS", "Bạn đã gửi đi báo cáo thành công");
-                handlerStatus = HandlerStatus.builder().status(true).content("Bạn đã gửi đi báo cáo thành công").build();
                 req.setAttribute("HOSTEL_OWNER_ID", ownerID);
                 req.setAttribute("REPORT_ID", reportId);
-                req.setAttribute("RESPONSE_MSG", handlerStatus);
-                url = SUCCESS;
+                handlerStatus = HandlerStatus.builder().status(true).content("Bạn đã gửi đi báo cáo thành công!").build();
+            } else {
+                handlerStatus = HandlerStatus.builder().status(false).content("Đã có lỗi xảy ra! Gửi báo cáo thất bại!").build();
             }
+
+            List<ReportCategory> reportCategories = new ReportCategoryDAO().getReportCategory();
+            req.setAttribute("REPORT_CATE", reportCategories);
+            req.setAttribute("RESPONSE_MSG", handlerStatus);
         } catch (Exception e) {
-            req.setAttribute("Fail", "Gửi báo cáo thất bại!");
+            req.setAttribute("RESPONSE_MSG", HandlerStatus.builder().status(false).content("Đã có lỗi xảy ra! Gửi báo cáo thất bại!").build());
             log("Error at AddReportServlet: " + e.toString());
         } finally {
-            req.getRequestDispatcher(url).forward(req, resp);
+            req.getRequestDispatcher("Renter-report-page").forward(req, resp);
         }
     }
 }
